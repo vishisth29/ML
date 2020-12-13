f70428c754e742ffde52cc1dbf3d45688c45d4fe
vishisth29 <mailtovishisth@gmail.com>
2020-12-13 02:02:07 -0500

    Update GAN.py

--- GAN.py
+++ GAN.py
@@ -1,4 +1,3 @@
-
 import torch
 import torch.nn as nn
 import numpy as np
@@ -6,5 +5,11 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import random
 import torchvision
-train = datasets.MNIST(root='../', train=True, transform=transforms.ToTensor(), download=True)
-test = datasets.MNIST(root='../', train=False, transform=transforms.ToTensor(), download=False)
+
+
+class Generator(torch.nn.Module):
+    def __init__(self):
+
+
+class Discriminator(torch.nn.Module):
+    def __init__(self):
