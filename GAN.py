8a4e07f01c2a820999130bdd9ae29b04071ab7eb
vishisth29 <mailtovishisth@gmail.com>
2020-12-13 02:00:37 -0500

    Update GAN.py

--- GAN.py
+++ GAN.py
@@ -6,3 +6,5 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import random
 import torchvision
+train = datasets.MNIST(root='../', train=True, transform=transforms.ToTensor(), download=True)
+test = datasets.MNIST(root='../', train=False, transform=transforms.ToTensor(), download=False)
